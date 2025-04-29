79bd72952f91f1a99881871e95b48a9b52a360f1
pengfei77 <169147327+pengfei77@users.noreply.github.com>
2025-04-29 21:59:28 +0800

    修改域名

--- src/index.js
+++ src/index.js
@@ -6,19 +6,15 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.dm66666.cn": "https://registry-1.docker.io",
+  "quay.dm66666.cn": "https://quay.io",
+  "gcr.dm66666.cn": "https://gcr.io",
+  "k8s-dm66666.cn": "https://k8s.gcr.io",
+  "k8s.dm66666.cn": "https://registry.k8s.io",
+  "ghcr.dm66666.cn": "https://ghcr.io",
+  "cloudsmith.dm66666.cn": "https://docker.cloudsmith.io",
 };
+
 
 function routeByHosts(host) {
   if (host in routes) {
